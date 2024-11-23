e246ae683d726fdc6bcd30b719140090657b8d0e
Minenhle Sithole <minenhlesithole@Minenhles-MacBook-Pro.local>
2024-11-23 14:57:08 +0200

    init commit

---
+++ src/test/kotlin/com/banking/api/kotlinbankingapi/KotlinBankingApiApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.banking.api.kotlinbankingapi
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KotlinBankingApiApplicationTests {
+
+  @Test
+  fun contextLoads() {
+  }
+
+}
