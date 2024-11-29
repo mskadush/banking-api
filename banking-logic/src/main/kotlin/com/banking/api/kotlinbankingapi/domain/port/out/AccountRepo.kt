660164eba7abb67e4dcb5913f1a08c7b95d704ef
mskadush <87610164+mskadush@users.noreply.github.com>
2024-11-29 06:49:22 +0200

    drafting w/ Lock

--- banking-logic/src/main/kotlin/com/banking/api/kotlinbankingapi/domain/port/out/AccountRepo.kt
+++ banking-logic/src/main/kotlin/com/banking/api/kotlinbankingapi/domain/port/out/AccountRepo.kt
@@ -3,6 +3,8 @@ package com.banking.api.kotlinbankingapi.domain.port.out
 import com.banking.api.kotlinbankingapi.domain.models.Account
 
 interface AccountRepo {
+  // TODO: 
+  // fun <T> withLock(accoutId: String, block: (Account) -> T): T 
   fun getAccount(accountId: String): Account?
   fun saveBalance(accountId: String, balance: Account): Account
 }
